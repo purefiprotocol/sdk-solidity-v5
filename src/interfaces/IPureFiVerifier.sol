f5b8d7bcfec564f3dfa0c369a9092af5f80d5d1b
DmitriyIschenko <dmitriysimit@gmail.com>
2026-02-01 20:06:11 +0100

    feat: added functions to the IPureFiVerifier.sol

--- src/interfaces/IPureFiVerifier.sol
+++ src/interfaces/IPureFiVerifier.sol
@@ -11,6 +11,7 @@ interface IPureFiVerifier {
 
     event PureFiPackageProcessed(address indexed caller, uint256 session);
     event PureFiStorageClear(address caller, uint256 sessionId);
+    event Withdrawn(address feeCollector, uint256 amount);
 
     error PureFiDataExpiredError();
     error TooShortPayloadError();
@@ -22,4 +23,8 @@ interface IPureFiVerifier {
     /// @dev Validates a given payload and returns the parsed package data.
     /// @param payload The payload to validate.
     function validatePayload(bytes calldata payload) external;
+    function withdraw(address account, uint256 amount) external;
+    function paidValidatePayload(bytes calldata _payload) external;
+    function validatePayload(bytes calldata _payload) external;
+    function clearStorage(uint256[] memory _sessions) external;
 }
